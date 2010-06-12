c6601a9188496ae494501c25613dd70e90148f51
Zhu.Yunchuan1 <Zhu.Yunchuan1@24104e14-fb2e-11de-8b2a-97a4b31bfdf8>
2010-06-12 12:26:07 +0000

    shrink

--- code/systerm_programmer/3_darray/darray.c
+++ code/systerm_programmer/3_darray/darray.c
@@ -45,9 +45,9 @@ static Ret darray_expand(DArray* thiz, size_t need)
 		
 		if((thiz->size) + nedd >= thiz->alloc_size)	
 		{
-				size_t alloc_size = (thiz->alloc_size>> 1) + thiz->alloc_size +MINI_RELLOC_SIZE;		
-				void** data = realloc(sizeof(void*) * alloc_size);
-		
+				size_t alloc_size = (thiz->alloc_size>> 1) + thiz->alloc_size + MINI_RELLOC_SIZE;		
+
+				void** data = (void**)realloc(sizeof(void*) * alloc_size);
 				if(NULL != data)
 				{
 						thiz->data = data;
@@ -56,4 +56,31 @@ static Ret darray_expand(DArray* thiz, size_t need)
 		}
 		
 		return  ((thiz->size) + need <= thiz->alloc_size) ? RET_OK : RET_FAIL;
+}
+
+static Ret darray_shrink(DArray* thiz) 
+{
+		return_val_if_fail(NULL != thiz, RET_INVALID_PARAMS);
+
+		if(thiz->alloc_size < (thiz->size >> 1) && thiz->size >= MINI_RELLOC_SIZE)
+		{
+				size_t size = thiz->alloc_size + alloc_size >> 1;
+
+				void** data = (void**)realloc(sizeof(void*) * size);
+				if(NULL != data)
+				{
+						thiz->size = size;
+						thiz->alloc_size = alloc_size;
+						thiz->data = data;
+				}
+		}
+
+		return RET_OK;
+}
+
+Ret darray_append(DArray* thiz, void* data)
+{
+		return_val_if_fail(NULL != thiz, RET_INVAILD_PARAMS);
+
+					
 }
