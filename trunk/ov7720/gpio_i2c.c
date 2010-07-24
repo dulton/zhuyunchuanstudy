fb7dfce7cff71fea2957eee61b371d4cdb66ae53
zhu.yunchuan1 <zhu.yunchuan1@24104e14-fb2e-11de-8b2a-97a4b31bfdf8>
2010-07-24 00:09:41 +0000

    ov7720驱动项目创建，完成了整体框架

---
+++ trunk/ov7720/gpio_i2c.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  gpio_i2c.c
+ *
+ *    Description:  gpio口模拟I2C协议
+ *
+ *        Version:  1.0
+ *        Created:  2010年07月22日 21时03分31秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+
+#include <gpio_i2c.h>
+#include <>
+
+static set_low(unsigned char gpio_pin)
+
+static set_high(unsigned char gpio_pin)
+
+static set_start_bit()
+
+static set_stop_bit()
+
+static set_ack(unsigned char dev_addr)
+
+static recive_ack(unsigned char dev_addr)
+
+static i2c_write_byte(unsigned char addr, unsigned char data)
+
+static i2c_read_byte(unsigned char dev_addr, unsigned char addr)
