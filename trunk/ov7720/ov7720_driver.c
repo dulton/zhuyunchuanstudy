5a6998213f37f07f762a11c2dd93591d30e91cc6
zhu.yunchuan1 <zhu.yunchuan1@24104e14-fb2e-11de-8b2a-97a4b31bfdf8>
2010-07-27 14:08:34 +0000

    完成init函数。

--- trunk/ov7720/ov7720_driver.c
+++ trunk/ov7720/ov7720_driver.c
@@ -34,8 +34,16 @@
 #include "ov7720.h"
 
 #define OV7720_NAME "ov7720"
-#define OV7720_MAJOR 144
-#define OV7720_MINOR 0
+
+static u32 ov7720_major = 144;
+static u32 ov7720_minor = 0;
+static 
+
+static int ov7720_open(struct inode *inode, struct file *filep);
+static int ov7720_release(struct inode *inode, struct file *filep);
+static int ov7720_ioctl(struct inode *inode, struct file *filep, int cmd, unsigned long arg);
+
+static OV7720* ov7720 = NULL;
 
 static struct ov7720_pin k6_pin = 
 {
@@ -44,4 +52,62 @@ static struct ov7720_pin k6_pin =
 	.RESET_PIN = K6_RESET_PIN,
 }
 
+struct file_operations ov7720_fops = 
+{
+	.owner = THIS_MODULE,
+	.open = ov7720_open,
+	.write = ov7720_write,
+	.ioctl = ov7720_ioctl,
+}
 
+/* 操作寄存器， 对ov7720进行初始化设置*/
+static int ov7720_dev_init()
+{
+}
+
+static int ov7720_init(void)
+{
+	dev_t dev = 0;
+	dev_t dev_num = 0;
+	int ret = -1;
+	//struct file_operations *ov7720_fops = NULL;
+
+		
+	dev = MKDEV(major, minor);
+	if((ret = register_chrdev_region(dev, 1, OV7720_NAME)) != 0)
+	{
+		printk("register ov7720 error");
+		return ret;
+	}
+	
+	dev_num = MKDEV(major, minor);
+	struct cdev *ov7720_dev = cdev_alloc();
+	if(NULL == ov7720_dev)
+	{
+		printk("alloc ov7720_dev erro");
+	}
+	ov7720_dev->fops = &ov7720_fops;
+	cdev_init(ov7720_dev, &ov7720_fops);
+	ov7720_dev->owner = THIS_MODULE;
+	kobject_set_name(&(ov7720_dev->kobj), "%s", OV7720_NAME);
+	if((ret = cdev_add(ov7720_dev, dev_num, 1)) != 0)
+	{
+		printk("ov7720 cdev_add error");
+	}
+
+	if(NULL == (ov7720 = ov7720_create(sccb_read, sccb_write, sccb_reset, k6_pin)))
+	{
+		printk("create ov7720 struct error");
+	}
+}
+
+static int ov7720_exit(void) 
+{
+	
+}
+
+module_init(ov7720_init)
+module_exit(ov7720_exit)
+
+MODULE_LICENCE("GPL")
+MODULE_AUTHOR("Zhu Yunchuan")
